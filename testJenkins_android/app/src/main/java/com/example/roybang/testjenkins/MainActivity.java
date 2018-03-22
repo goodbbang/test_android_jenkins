6120ce46152052bbfa294525b758249eaff5bec4
Roy Bang <roy.bang@wirebarley.com>
2018-03-22 12:46:38 +0900

    ddd

--- testJenkins_android/app/src/main/java/com/example/roybang/testjenkins/MainActivity.java
+++ testJenkins_android/app/src/main/java/com/example/roybang/testjenkins/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//test
-    //ssss
-    // test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
